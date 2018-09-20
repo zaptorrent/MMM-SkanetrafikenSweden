00574d9e4026f63f8388db54f3f60da30241f0ed
zaptorrent <31840323+zaptorrent@users.noreply.github.com>
2018-09-21 01:15:44 +0200

    Update

--- MMM-SkanetrafikenSweden.js
+++ MMM-SkanetrafikenSweden.js
@@ -1,31 +1,44 @@
-/* MMM-CrisisInformationSweden.js
+/* MMM-SkanetrafikenSweden.js
  *
- * Magic Mirror module - News feed from the Swedish Government Crisis Information (Krisinformation.se). 
+ * Magic Mirror module - Train & bus departures from the Skånetrafiken API (http://labs.skanetrafiken.se/api.asp). 
  * 
  * Magic Mirror
- * Module: MMM-CrisisInformationSweden
+ * Module: MMM-SkanetrafikenSweden
  * 
  * Magic Mirror By Michael Teeuw http://michaelteeuw.nl
  * MIT Licensed.
  * 
- * Module MMM-CrisisInformationSweden By Anders Boghammar
+ * Module MMM-SkanetrafikenSweden By Lee Embrin
  * 
  * Notifications:
  *      CONFIG: Sent to update any listeners of the current configuration.
- *      NEW_FEED: Received when a new feed is available.
+ *      NEW_DEPARTURE: Received when a new feed is available.
  *      SERVICE_FAILURE: Received when the service access failed.
+ *
+ * API Example response
+ *      Deviations: [""]
+ *      IsTimingPoint: ["false"]
+ *      JourneyDateTime: ["2018-08-11T14:03:00"]
+ *      LineTypeId: ["4"]
+ *      LineTypeName: ["Stadsbuss"]
+ *      Name: ["6"]
+ *      No: ["6"]
+ *      RealTime: [{…}]
+ *      RunNo: ["264"]
+ *      StopPoint: ["B"]
+ *      Towards: ["Klagshamn via Värnhem"]
+ *      TrainNo: ["0"]
  */
-Module.register("MMM-CrisisInformationSweden", {
+Module.register("MMM-SkanetrafikenSweden", {
     // --------------------------------------- Define module defaults
     defaults: {
-        alwaysNational: true,           // Optional, Regardless of other settings always show national info. Not implemented yet
-        updateInterval: 30*60*1000,     // Optional. Number of ms between API updates. 
-        uiUpdateInterval: 10*1000,      // Optional. Number of ms between changing to next announcement. 
-        areas: [],                      // Optional. An array of strings with area names. 
-                                        // Only those messages aimed at the areas listed in the array are shown. 
-                                        // If empty or undefined show all messages.
-        showDescription: true,          // Optional. Show message description. Not yet implemented.
-        oldest: 7,                      // Optional. Dont show messages older then this number of days.
+        updateInterval: 10*60*1000,     // Optional. Number of ms between API updates. 
+        uiUpdateInterval: 10*1000,      // Optional. Number of ms between updating UI. 
+        oldest: 30,                     // Optional. Dont show departures further away than this number of minutes.
+        maxResults: 10,                 // Optional. Max no of results to show. Can't be more than 50.
+        stationId: "80000",             // Required. The id of the stop you want to monitor. Find your id: 
+                                        // http://www.labs.skanetrafiken.se/v2.2/querystation.asp?inpPointfr=hyllie 
+                                        // (change "hyllie" in url to the name of your stop). 
     },
     
     // --------------------------------------- Define required scripts
@@ -53,94 +66,33 @@ Module.register("MMM-CrisisInformationSweden", {
 
     // --------------------------------------- Generate dom for module
     getDom: function() {
+        var self = this;
         var wrapper = document.createElement("div");
 
+        // ----- Show info if we haven't had any response yet
         if (!this.loaded) {
-			wrapper.innerHTML = this.name + " loading feeds ...";
+			wrapper.innerHTML = this.name + " loading departures ...";
 			wrapper.className = "dimmed light small";
 			return wrapper;
 		}
 
-        // ------ Display a selected message in the feed
-        if (this.currentFeedIndex >= this.currentFeed.length) this.currentFeedIndex = 0;
-        if (this.currentFeed.length > 0) { // We have messages display the one up for displaying
-            this.debug('Trying to display feed ix: '+this.currentFeedIndex);
-            var noFeedsToDisplay = false;
-            var dt = moment(this.currentFeed[this.currentFeedIndex].Published);
-            if (moment().diff(dt) > this.config.oldest*24*60*60*1000) {
-                noFeedsToDisplay = this.currentFeedIndex == 0;
-                this.currentFeedIndex = 0;
+        // ----- Show departures
+        var maxTime = moment().add(self.config.oldest,"m").valueOf(),
+            between = false,
+            now = new Date();
+        
+        self.currentFeed.forEach(function(msg,i){           
+            deptTime = new Date(msg.JourneyDateTime[0]);
+            between = moment(deptTime).isBetween(now, maxTime);
+            
+            // Filter out the departures to display
+            if (between && (i<self.config.maxResults)) {
+                var ddiv = document.createElement("div");
+                ddiv.className = 'dimmed xsmall';
+                ddiv.innerHTML = msg.LineTypeName[0] + " " + msg.Name[0] + " towards " + msg.Towards[0] + " " + moment(msg.JourneyDateTime[0]).fromNow();
+                wrapper.appendChild(ddiv);
             }
-            this.debug('Feed ix: '+this.currentFeedIndex + " noFeedsToDisplay: "+ noFeedsToDisplay);
-            if (noFeedsToDisplay) {
-                var div = document.createElement("div");
-                div.innerHTML = this.name + ': There are no messages younger than '+this.config.oldest + ' days';
-                //div.style.color = "red"; // TODO Change this to a custom style
-                div.className = 'dimmed xsmall';
-                wrapper.appendChild(div);
-            } else {
-                this.debug('Display feed ix: '+this.currentFeedIndex);
-
-                var msg = this.currentFeed[this.currentFeedIndex];
-
-                var tdiv = document.createElement("div");
-                tdiv.className = 'align-left';
-                var spant = document.createElement("div");
-                spant.innerHTML = moment(msg.Published).fromNow() + " " // TODO Format the time according to how long ago it was
-                    + (this.config.debug 
-                        ? moment().format('HH:mm:ss') + ' Ix:'+ this.currentFeedIndex + ' Pub: '+msg.Published 
-                        :'');
-                spant.className = 'dimmed xsmall';
-                tdiv.appendChild(spant);
-
-                var spanh = document.createElement("div");
-                spanh.innerHTML = msg.InfoData[0].Headline;
-                spanh.className = 'small align-left';
-                tdiv.appendChild(spanh);
-                wrapper.appendChild(tdiv);
-
-                if (this.config.showDescription) {
-                    var ddiv = document.createElement("div");
-                    ddiv.innerHTML = msg.InfoData[0].Description;
-                    ddiv.className = 'dimmed xsmall align-left';
-                    wrapper.appendChild(ddiv);
-                }
-                var bdiv = document.createElement("div");
-                bdiv.className = 'dimmed xsmall';
-                // TODO use style instead
-                bdiv.style.marginTop = '5px';
-                bdiv.style.borderTopWidth = '1px';
-                bdiv.style.borderTopColor = '#666';
-                bdiv.style.borderTopStyle = 'dotted';
-                if (msg.InfoData[0].Area !== undefined && msg.InfoData[0].Area != null && msg.InfoData[0].Area.length > 0) {
-                    var adiv = document.createElement("span");
-                    adiv.innerHTML = '<b>Area(s):</b> ';
-                    for (var ia = 0 ; ia < msg.InfoData[0].Area.length; ia++) {
-                        adiv.innerHTML = adiv.innerHTML + (ia > 0 ? ', ' : '') + msg.InfoData[0].Area[ia].AreaDesc;
-                    }
-                    adiv.className = 'align-left';
-                    adiv.style.cssFloat = 'left';
-                    bdiv.appendChild(adiv);
-                }
-                if (this.config.debug) {
-                    var sdiv = document.createElement("span");
-                    sdiv.innerHTML = '<b>Feeds:</b> ' + this.currentFeed.length;
-                    //sdiv.className = 'align-right';
-                    //sdiv.style.cssFloat = 'right';
-                    bdiv.appendChild(sdiv);
-                }
-                if (msg.InfoData[0].SenderName !== undefined && msg.InfoData[0].SenderName != '') {
-                    var sdiv = document.createElement("span");
-                    sdiv.innerHTML = '<b>From:</b> ' + msg.InfoData[0].SenderName;
-                    sdiv.className = 'align-right';
-                    sdiv.style.cssFloat = 'right';
-                    bdiv.appendChild(sdiv);
-                }
-                wrapper.appendChild(bdiv);
-
-                this.currentFeedIndex++; // On to next feed if any
-            }
-        }
+        });
 
         // ----- Show service failure if any
         if (this.failure !== undefined) {
@@ -157,15 +109,15 @@ Module.register("MMM-CrisisInformationSweden", {
     debug: function(msg) {
         if (this.config.debug) console.log(this.name + ': ' + msg);
     },
-
+    
     // --------------------------------------- Handle socketnotifications
     socketNotificationReceived: function(notification, payload) {
-        if (notification === "NEW_FEED") {
+        if (notification === "NEW_DEPARTURE") {
             this.loaded = true;
             this.failure = undefined;
             // Handle payload
             this.currentFeed = payload;
-            Log.info("New feed updated: "+ this.currentFeed.length);
+            Log.info("Departures updated: "+ this.currentFeed.length);
             this.updateDom();
         }
         if (notification == "SERVICE_FAILURE") {
--- node_helper.js
+++ node_helper.js
@@ -1,17 +1,37 @@
 /* node_helper.js
  *
- * Magic Mirror module - News feed from the Swedish Government Crisis Information (Krisinformation.se). 
+ * Magic Mirror module - Train & bus departures from the Skånetrafiken API (http://labs.skanetrafiken.se/api.asp). 
  * 
  * Magic Mirror
- * Module: MMM-CrisisInformationSweden
+ * Module: MMM-SkanetrafikenSweden
  * 
  * Magic Mirror By Michael Teeuw http://michaelteeuw.nl
  * MIT Licensed.
  * 
- * Module MMM-CrisisInformationSweden By Anders Boghammar
+ * Module MMM-SkanetrafikenSweden By Lee Embrin
+ * 
+ * Notifications:
+ *      CONFIG: Sent to update any listeners of the current configuration.
+ *      NEW_DEPARTURE: Received when a new feed is available.
+ *      SERVICE_FAILURE: Received when the service access failed.
+ *
+ * API Example response
+ *      Deviations: [""]
+ *      IsTimingPoint: ["false"]
+ *      JourneyDateTime: ["2018-08-11T14:03:00"]
+ *      LineTypeId: ["4"]
+ *      LineTypeName: ["Stadsbuss"]
+ *      Name: ["6"]
+ *      No: ["6"]
+ *      RealTime: [{…}]
+ *      RunNo: ["264"]
+ *      StopPoint: ["B"]
+ *      Towards: ["Klagshamn via Värnhem"]
+ *      TrainNo: ["0"]
  */
 const NodeHelper = require("node_helper");
 const request = require("request-promise");
+const nodeSkanetrafiken = require('node-skanetrafiken');
 
 module.exports = NodeHelper.create({
     // --------------------------------------- Start the helper
@@ -28,54 +48,21 @@ module.exports = NodeHelper.create({
         }, self.config.updateInterval);
     },
 
-    // --------------------------------------- Retrive new feed
+    // --------------------------------------- Get the feed
     getFeed: function() {
         var self = this;
-        console.log((new Date(Date.now())).toLocaleTimeString() + ': Getting feed for module ' + this.name);
-        var opt = {
-            uri: 'http://api.krisinformation.se/v1/capmessage?format=json',
-            //uri: 'http://api.krisinformation.se/v1/feed',
-            qs : {
-            },
-            json: true
-        };
-        console.log('Calling '+opt.uri);
-        request(opt)
-            .then(function(resp) {
-                var feeds = self.filterFeed(resp);
-                console.log((new Date(Date.now())).toLocaleTimeString() 
-                    + ": "+ this.name +" - Sending NEW_FEED count: "+feeds.length + " Org: " + resp.length);
-                self.sendSocketNotification('NEW_FEED', feeds); // Send feed to module
-            })
-            .catch(function(err) {
-                console.log('Problems with '+ this.name +': '+err);
-                self.sendSocketNotification('SERVICE_FAILURE', {resp: {StatusCode: 600, Message: err}}); 
-            });
-    },
 
-    // --------------------------------------- Filter feeds according to config
-    filterFeed: function(resp) {
-        if (this.config.areas === undefined || this.config.areas.length < 1) return resp;
-        var feeds = [];
-        for (var ix = 0; ix < resp.length; ix++) {
-            var inc = false;
-            var feed = resp[ix];
-            var areas = feed.InfoData[0].Area;
-            //console.log("Looking at "+ feed.Identifier);
-            if (areas === undefined || areas === null) inc = true; // Always include iof there's no area defined
-            else {
-                for (var ia = 0; ia < areas.length; ia++) {
-                    for (var iad = 0; iad < this.config.areas.length; iad++) {
-                        if (areas[ia].AreaDesc == this.config.areas[iad]) inc = true;
-                    }
-                    if (this.config.alwaysNational && areas[ia].AreaDesc == "Sverige") inc = true;
-                }
+        nodeSkanetrafiken.getDepartures({ stopID: self.config.stationId }, function(results, err) {
+           // Do something with the results
+            if(results) {
+                self.sendSocketNotification('NEW_DEPARTURE', results);
             }
-            if (inc) feeds.push(feed);
-        }
-        return feeds;
+            if(err) {    
+                self.sendSocketNotification('SERVICE_FAILURE', err);
+            }
+        });
     },
-
+    
     // --------------------------------------- Handle notifications
     socketNotificationReceived: function(notification, payload) {
         const self = this;
